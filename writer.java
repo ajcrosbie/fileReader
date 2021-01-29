e35ae6e236caf87350434352dd5a6f7262f8f232
Aaron Crosbie <aaroncrosie@outlook.com>
2021-01-29 13:19:01 +0000

    file writer now adds random integers

---
+++ writer.java
@@ -0,0 +1,26 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class writer {
+    public static void main(String[] args) {
+        try {
+            FileWriter F = new FileWriter("File.txt");
+            for (int i = 0; i < 5; i++) {
+                int x = generateRandom();
+                F.write(new Integer(x).toString());
+            }
+            F.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public static int generateRandom() {
+        Random rand = new Random();
+        int randint = rand.nextInt(100);
+        return randint;
+    }
+
+}
