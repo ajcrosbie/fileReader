0ee3250d1622058d74199c5278b836e4d7e9d48d
Aaron Crosbie <aaroncrosie@outlook.com>
2021-01-29 09:16:41 +0000

    int read and average calculation added

--- reader.java
+++ reader.java
@@ -4,19 +4,23 @@ import java.util.Scanner;
 
 public class reader {
     public static void main(String[] args) {
+        int g = 0;
+        int count = 0;
         try {
             File ob = new File("File.txt");
             Scanner reader1 = new Scanner(ob);
             while (reader1.hasNextLine()) {
-                String data = reader1.nextLine();
-                System.out.println(data);
-
+                int ci = reader1.nextInt();
+                count = count + 1;
+                g = ci + g;
             }
+            g = g / count;
             reader1.close();
         } catch (FileNotFoundException e) {
             System.out.println("an error occurred");
             e.printStackTrace();
         }
+        System.out.println(g);
 
     }
 }
